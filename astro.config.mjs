4c40781f5753e2594afd6688cee62b4a39151670
Mathew Teague <18143654+YewTreeWeb@users.noreply.github.com>
2024-05-18 13:36:58 +0100

    feat: added toml and icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://yewtreeweb.co.uk/",
   integrations: [tailwind()],
-  vite: {
-    ssr: {
-      external: ["svgo"],
-    },
-  },
+  output: "server",
+  adapter: netlify(),
 });
